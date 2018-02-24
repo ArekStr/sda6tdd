e64470c3830b4078a8147b5849b692347a6a9124
Arek Strenk <lechpoznan12@gmail.com>
2018-02-24 11:24:15 +0100

    capitalized named functionality

--- src/test/java/com/sda/WriterTest.java
+++ src/test/java/com/sda/WriterTest.java
@@ -1,12 +1,19 @@
 package com.sda;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WriterTest {
+    private Writer writer;
+    @Before
+    public void init() {
+        writer = new Writer(); //metoda odpali sie w kazdym  tescie
+
+    }
     @Test
     public void sayHelloWithGivenNameTest(){
-        Writer writer = new Writer();
+
         String name = "Arek";
         String resault = writer.sayHello(name);
 
@@ -17,17 +24,27 @@ public class WriterTest {
 
     public void sayHelloWithNoNameSpecified(){
 
-        Writer writer = new Writer();
+
         String name="";
         String result = writer.sayHello("");
         Assert.assertEquals("Message for emtpy name should be different", "Hello my friend!", result);
 
     }
      public void sayHelloWithNullName(){
-        Writer writer = new Writer();
         String name = null;
         String result = writer.sayHello(name);
         Assert.assertEquals("Hello, my friend!", result);
      }
+
+     @Test
+
+    public void sayHelloWithCapitalizedNameSpecified(){
+         String name = "AREK";
+         String result = writer.sayHello(name);
+         Assert.assertEquals("HELLO, AREK!", result);
+
+
+     }
+
 }
 
